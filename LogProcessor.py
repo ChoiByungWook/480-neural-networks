83b9dbbb3e1c0b7d939b335c8d80327648ab7998
dchoi1 <dchoi1@cpp.edu>
2017-03-15 22:48:36 -0700

    Copied original log file to allow for classification

--- LogProcessor.py
+++ LogProcessor.py
@@ -1,19 +1,23 @@
 import fileinput
+import shutil
 import sys
 
 import ServerLogConverter
 
 LOG_PATH = "logs/"
+MARKED = "marked_"
 
 
 def main():
     file = str(sys.argv[1])
     log_file = LOG_PATH + file
 
+    # create marked file
+    marked_file = LOG_PATH + MARKED + file
+    shutil.copyfile(log_file, marked_file)
+
     for line in fileinput.input(log_file):
         server_log_tuple = ServerLogConverter.convert_line_to_server_log_tuple(line)
-        print(server_log_tuple)
-        break
 
     fileinput.close()
 
