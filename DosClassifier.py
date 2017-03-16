6b5fc295b7088a4b7f938a85830e16088f99db75
Kevin Grossi <kmgrossi@cpp.edu>
2017-03-16 14:18:53 -0700

    Added threshold, still need refinement

--- DosClassifier.py
+++ DosClassifier.py
@@ -1,5 +1,6 @@
 import pprint
 
+
 class DosClassifier:
     # Dictionary that keeps track of the frequency of the requests at hourly intervals.
     interval_frequency = {}
@@ -16,10 +17,9 @@ class DosClassifier:
     will be more reliable after analyzing previous data stored in interval_frequency.
     '''
     def classify(my_tuple):
-        # This defines the weight of the features relative to the classifier.
-        # weights_of_features = {my_tuple[0]: 0, my_tuple[1]: 0, my_tuple[2]: 0,
-        #                        my_tuple[3]: 3, my_tuple[4]: 0, my_tuple[5]: 0,
-        #                        my_tuple[6]: 0, my_tuple[7]: 0, my_tuple[8]: 0}
+        # Score to be determined after classification criteria are considered.
+        overall_score = 0
+        threshold = 3
 
         # Check the time of the current log to add it to the corresponding key:value pair in interval_frequency
         if my_tuple.time.hour not in DosClassifier.interval_frequency:
@@ -38,21 +38,20 @@ class DosClassifier:
         for i in range(0, len(value_list)):
             sum += value_list[i]
 
-        average = sum/len(value_list)
-
-        print(average) #Right now this is ~1920
+        average = sum / len(value_list)
 
         ''' Criteria for determining a DOS attack:
             -If the duration of session is less than 1 second
             -If the time of request is less than the usual average of the log file
+            -If the set of the previous 5 logs is equal to 1
         '''
-        if my_tuple.duration.second < 3 and DosClassifier.interval_frequency[my_tuple.time.hour] < average:
+        if my_tuple.duration.second < 3:
+            overall_score += 2
+
+        if DosClassifier.interval_frequency[my_tuple.time.hour] <= average:
+            overall_score += 1
+
+        if overall_score >= threshold:
             return True
         else:
             return False
-
-
-
-
-
-
