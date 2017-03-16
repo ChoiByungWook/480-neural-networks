f032e745a91f70e2da82bfe2f9c3207121a880a9
dchoi1 <dchoi1@cpp.edu>
2017-03-15 22:39:39 -0700

    Refactored ServerLogConverter

--- LogProcessor.py
+++ LogProcessor.py
@@ -1,38 +1,22 @@
-import collections
 import fileinput
-import datetime
 import sys
 
+import ServerLogConverter
+
+LOG_PATH = "logs/"
+
+
 def main():
-    log_file = str(sys.argv[1])
+    file = str(sys.argv[1])
+    log_file = LOG_PATH + file
 
     for line in fileinput.input(log_file):
-        server_log_tuple = convert_line_to_server_log_tuple(line)
+        server_log_tuple = ServerLogConverter.convert_line_to_server_log_tuple(line)
+        print(server_log_tuple)
+        break
 
-def convert_line_to_server_log_tuple(line):
-    ServerLogTuple = collections.namedtuple('log_line', ['number', 'date', 'time', 'duration', 'protocol', 'src_port', 'destination_port', 'src_ip', 'destination_ip'])
-    split_line = line.split(" ")
+    fileinput.close()
 
-    number = split_line[0]
-
-    dateMDY = split_line[1]
-    date = datetime.datetime.strptime(dateMDY, "%m/%d/%Y").date()
-
-    timeHMS = split_line[2]
-    time  = datetime.datetime.strptime(timeHMS, "%X").time()
-
-    durationHMS = split_line[3]
-    duration = datetime.datetime.strptime(durationHMS, "%X").time()
-
-    protocol = split_line[4]
-    src_port = split_line[5]
-    destination_port = split_line[6]
-    src_ip = split_line[7]
-    destination_ip = split_line[8]
-
-    server_log_tuple = ServerLogTuple(number, date, time, duration, protocol, src_port, destination_port, src_ip, destination_ip)
-
-    return server_log_tuple
 
 if __name__ == '__main__':
     main()
---
+++ ServerLogConverter.py
@@ -0,0 +1,36 @@
+import collections
+
+import datetime
+
+
+def convert_line_to_server_log_tuple(line):
+    ''' Converts line of text from a server log into a named tuple representation.
+
+    :param line: Line of server log text
+    :return: Named tuple representation of line
+    '''
+    ServerLogTuple = collections.namedtuple('log_line', ['number', 'date', 'time', 'duration', 'protocol', 'src_port',
+                                                         'destination_port', 'src_ip', 'destination_ip'])
+    split_line = line.rstrip().split(" ")
+
+    number = split_line[0]
+
+    dateMDY = split_line[1]
+    date = datetime.datetime.strptime(dateMDY, "%m/%d/%Y").date()
+
+    timeHMS = split_line[2]
+    time = datetime.datetime.strptime(timeHMS, "%X").time()
+
+    durationHMS = split_line[3]
+    duration = datetime.datetime.strptime(durationHMS, "%X").time()
+
+    protocol = split_line[4]
+    src_port = split_line[5]
+    destination_port = split_line[6]
+    src_ip = split_line[7]
+    destination_ip = split_line[8]
+
+    server_log_tuple = ServerLogTuple(number, date, time, duration, protocol, src_port, destination_port, src_ip,
+                                      destination_ip)
+
+    return server_log_tuple
